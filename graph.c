f4c6baf30aba969681b2aa023bf4dd5167abf56f
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2020-12-15 16:04:31 +0200

    Made the graph in C a bit more understandable

--- graph.c
+++ graph.c
@@ -8,15 +8,10 @@ struct vertex_t
     struct vertex_t* next;
 };
 
-struct adj_list_t
-{
-    struct vertex* start_of_vertex_arr;
-};
-
 typedef struct graph_t 
 {   
     uint8_t size;
-    struct adj_list_t* adj_list;
+    struct vertex_t** adj_list;
 }graph_t;
 
 struct vertex_t* create_node(uint8_t value);
@@ -26,13 +21,9 @@ void add_edge(graph_t* graph, uint8_t node1, uint8_t node2);
 int main()
 {
     graph_t* graph = create_graph(10);
-    add_edge(graph, 0, 1);
-    uint8_t* arr = graph->adj_list[0].start_of_vertex_arr;
-    while (*arr)
-    {
-        printf("%d->", *arr);
-        arr++;
-    }
+    add_edge(graph, 1, 0);
+    struct vertex_t** list = graph->adj_list;
+    printf("%d\n", list[0]->v);
 }
 
 struct vertex_t* create_node(uint8_t value)
@@ -43,23 +34,27 @@ struct vertex_t* create_node(uint8_t value)
     return res;
 }
 
-struct graph_t* create_graph(uint8_t s)
+struct graph_t* create_graph(uint8_t size)
 {
     struct graph_t* graph = malloc(sizeof(graph_t));
-    graph->size = s;
-    graph->adj_list = malloc(sizeof(struct adj_list_t) * s);
+    graph->size = size;
+    graph->adj_list = malloc(sizeof(struct vertex_t) * size);
 
-    for (uint8_t i = 0; i < s; i++)
-        graph->adj_list[i].start_of_vertex_arr = NULL;
+    for (uint8_t i = 0; i < size; i++)
+        graph->adj_list[i] = NULL;
     return graph;
 }
 
-void add_edge(graph_t* graph, uint8_t node1, uint8_t node2)
+void add_edge(graph_t* graph, uint8_t s, uint8_t d)
 {
-    struct vertex_t* val1 = create_node(node1);
-    struct vertex_t* val2 = create_node(node2);
-    val1->next = graph->adj_list[node2].start_of_vertex_arr; // This puts the pointer to the adj list in node2 in the next value for val1
-    graph->adj_list[node2].start_of_vertex_arr = val1;  // Asigns the value
-    val2->next = graph->adj_list[node1].start_of_vertex_arr;
-    graph->adj_list[node1].start_of_vertex_arr = val2;
+    struct vertex_t* src = create_node(s);
+    struct vertex_t* dest = create_node(d);
+    
+    /* Adding a edge from src to dest */
+    dest->next = graph->adj_list[s];
+    graph->adj_list[s] = dest;
+
+    /* Adding a edge from dest to src */
+    src->next = graph->adj_list[d];
+    graph->adj_list[d] = src;
 }
