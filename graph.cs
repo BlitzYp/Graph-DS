6b1d04f97ba00042607db8802fea2435a60da9a4
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2020-12-16 21:04:50 +0200

    Added bfs

--- graph.cs
+++ graph.cs
@@ -3,33 +3,49 @@ using System.Collections.Generic;
 using System.Collections;
 
 class Program {
-    class Graph
+  class Graph
+  {
+    public LinkedList<int>[] AdjList;
+    public Graph(int size) 
     {
-        public LinkedList<int>[] AdjList;
-
-        public Graph(int size) 
-        {
-          AdjList = new LinkedList<int>[size];
-
-          for (int i = 0; i < size; i++) {
-            AdjList[i] = new LinkedList<int>();
-          }
-        }
-        public void AddEdge(int node1, int node2)
-        {
-          AdjList[node1].AddLast(node2);
-          AdjList[node2].AddLast(node1);
-        }
-    }
-
-    public static void Main(string[] args)
-    {
-      Graph graph = new Graph(5);
-      graph.AddEdge(0, 1);
-      graph.AddEdge(0, 2);
-      graph.AddEdge(1, 2);
-      foreach (int i in graph.AdjList[1]) {
-        Console.WriteLine(i);
+      AdjList = new LinkedList<int>[size];
+      for (int i = 0; i < size; i++) {
+        AdjList[i] = new LinkedList<int>();
       }
     }
+
+    public void Bfs(int StartNode) 
+    {
+      Queue<int> queue = new Queue<int>();
+      HashSet<int> visited = new HashSet<int>();
+      queue.Enqueue(StartNode);
+      visited.Add(StartNode);
+
+      while (queue.Count != 0) 
+      {
+        int Vertex = queue.Dequeue();
+        Console.WriteLine(Vertex);
+        foreach (int i in AdjList[Vertex]) {
+          if (!visited.Contains(i)) {
+            visited.Add(i);
+            queue.Enqueue(i);
+          }
+        }
+      }
+    }
+    public void AddEdge(int node1, int node2)
+    {
+      AdjList[node1].AddLast(node2);
+      AdjList[node2].AddLast(node1);
+    }
+  }
+  public static void Main(string[] args)
+  {
+    Graph graph = new Graph(5);
+    graph.AddEdge(0, 1);
+    graph.AddEdge(0, 2);
+    graph.AddEdge(1, 2);
+    graph.Bfs(0);
+  }
 }
+
