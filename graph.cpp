e3a64233bb6870f3717dea60e38f0a71d4c212ac
BlitzYp <63169496+BlitzYp@users.noreply.github.com>
2021-01-20 13:12:44 +0200

    Made the graph more understandable

--- graph.cpp
+++ graph.cpp
@@ -1,90 +1,76 @@
 #include <iostream>
-#include <map>
 #include <vector>
+#include <map>
 #include <set>
 #include <queue>
 
-class Graph {
-    public:
-        std::map<int, std::vector<int>> adject_list;
+class Graph
+{
+public:
+	std::map<int, std::vector<int>> data;
+	size_t size;
+	Graph()
+	{
+		data = std::map<int, std::vector<int>>();
+		size = 0;
+	}
 
-        template<typename T>
-        void add_vertex(const T num1)
-        {
-            std::vector<T> neighbors;
-            adject_list[num1] = neighbors;
-        }
+	void addVertex(int n)
+	{
+		data.insert(std::pair<int, std::vector<int>>(n, std::vector<int>(1)));
+	}
 
-        template<typename T>
-        void add_edge(const T node1, const T node2)
-        {
-            adject_list[node1].push_back(node2);
-            adject_list[node2].push_back(node2);
-        }
+	void addEdge(int n, int a)
+	{
+		data[n].push_back(a);
+		data[a].push_back(n);
+	}
+
+	void showEdges()
+	{
+		for (auto i: data) 
+		{
+			std::vector<int> currentData = i.second;
+			printf("%d", i.first);
+			for (auto j: currentData) 
+			{
+				if (j != 0) printf("->%d", j);
+			}	
+			puts(" ");
+		}
+	}
+
+	void breadthFirstSearch(int x, std::vector<int>& placeToStore)
+	{
+		std::set<int> visited = std::set<int>();
+		std::queue<int> q = std::queue<int>();
+		q.push(x);
+		placeToStore.push_back(x);
+		while (!q.empty())
+		{
+			int current = q.front();
+			q.pop();
+			for (auto i : data[current])
+			{
+				if (!visited.count(i))
+				{
+					visited.insert(i);
+					placeToStore.push_back(i);
+				}
+			}
+		}
+	}
 };
-
-void dfs(Graph* graph, const int node, std::set<int> visited, std::vector<int>* res)
-{
-    if (!node)
-    {   
-        return;
-    }
-
-    visited.insert(node);
-    res->push_back(node);
-
-    for (auto i: graph->adject_list[node])
-    {
-        if (!visited.count(i))
-        {
-            dfs(graph, i, visited, res);
-        }
-    }
-}
-
-template<typename T>
-void bfs(Graph* graph, const T node, std::vector<int>& res)
-{
-    std::set<T> visited;
-    std::queue<T> queue;
-
-    queue.push(node);
-    visited.insert(node);
-
-    while (queue.size() != 0)
-    {
-        T vertex = queue.front();
-        queue.pop();
-        res.push_back(vertex);
-        for (auto i: graph->adject_list[vertex])
-        {
-            if (!visited.count(i))
-            {
-                queue.push(i);
-                visited.insert(i);
-            }
-        }
-    }
-}
 
 int main()
 {
-    Graph graph;
-    graph.add_vertex<int>(4);
-    graph.add_vertex<int>(3);
-    graph.add_vertex<int>(5);
-    graph.add_vertex<int>(10);
-    graph.add_edge<int>(4, 3);
-    graph.add_edge<int>(10, 5);
-    graph.add_edge<int>(5, 3);
-    graph.add_edge<int>(10,4);
-    graph.add_edge<int>(3, 10);
-    std::vector<int> res;
-    //dfs(&graph, 4, std::set<int>(), &res);
-    bfs(&graph, 10, res);
-    
-    for (auto i: res) 
-    {
-        std::cout << i << "\n";
-    }
+	Graph graph = Graph();
+	graph.addVertex(2);
+	graph.addVertex(4);
+	graph.addEdge(2, 4);
+	graph.showEdges();
+	std::vector<int> data;
+	graph.breadthFirstSearch(2, data);
+	for (auto i : data)
+		std::cout << i << '\n';
 }
